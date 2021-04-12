6119a5f484f335b516d40dd48fbe7809a24a8ff3
beckcavu <beckcavu@umich.edu>
2021-04-12 17:03:47 -0400

    items draggable into screen

---
+++ pat
@@ -0,0 +1,1 @@
+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
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,6 +5,9 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
@@ -12,6 +15,8 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
+import sun.security.ec.point.Point;
 
 @SuppressWarnings("serial")
 public class GamePanel extends JPanel implements ActionListener{
@@ -28,12 +33,25 @@ public class GamePanel extends JPanel implements ActionListener{
 		public int rank, section, X, Y;
 		public Image sprite;
 	}
+	public class monkey_models {
+		public int X;
+		public int Y;
+		public Image sprite;
+	}
+	public class dart_monkey_model extends monkey_models {
+		dart_monkey_model() {
+			X = 675;
+			Y = 50;
+			sprite = dart_monkey.getImage();
+		}
+	}
 	
 	//members
 	final int RED = 1;
 	final int BLUE = 2;
 	final int NUM_ROUNDS = 1;
 	ArrayList<bloon> round_collection;
+	ArrayList<monkey_models> monkeys;
 	JButton start_round;
 	JButton dart_monke;
 	JLabel money;
@@ -42,6 +60,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	public int spawnY;
 	final public int BLOON_SIZE = 25;
 	public Boolean running = false;
+	public Boolean model;
 	Timer timer;
 	final Image red = new ImageIcon("images/red_bloon.png").getImage();
 	final Image blue = new ImageIcon("images/blue_bloon.png").getImage();
@@ -55,22 +74,64 @@ public class GamePanel extends JPanel implements ActionListener{
 		this.setLayout(null);
 		this.setBackground(Color.white);
 		round_in_play = 0;
+		model = false;
 		spawnX = 0;
 		spawnY = 245;
 		timer = new Timer(1,this);
 		round_collection = new ArrayList<bloon>();
+		monkeys = new ArrayList<monkey_models>();
 		build_round_collection(round_in_play);
 		
 		//MENU stuff
 		dart_monke = new JButton(dart_monkey);
 		dart_monke.setBounds(675,50,50,45);
-		dart_monke.addActionListener(new ActionListener() {
+		dart_monke.addMouseListener(new MouseListener() {
 
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			//must be overriden, does nothing
+			public void mouseClicked(MouseEvent e) {
+				//must be overriden, does nothing
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				dart_monkey_model bob = new dart_monkey_model();
+				monkeys.add(bob);
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
 				money.setText(String.valueOf(Integer.parseInt(money.getText()) - 150));
 			}
-			
+
+			@Override
+			//must be overriden, does nothing
+			public void mouseEntered(MouseEvent e) {
+				//must be overriden, does nothing
+				
+			}
+
+			@Override
+			//must be overriden, does nothing
+			public void mouseExited(MouseEvent e) {
+				//must be overriden, does nothing
+				
+			}
+		
+		});
+		dart_monke.addMouseMotionListener(new MouseMotionListener() {
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				model = true;
+				moveModel(e.getX(),e.getY());
+			}
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				//must be here, does nothing
+			}
 		});
 		money = new JLabel("750", cash, JLabel.CENTER);
 		money.setFont(new Font("SansSerif", Font.PLAIN, 30));
@@ -90,6 +151,13 @@ public class GamePanel extends JPanel implements ActionListener{
 		this.setVisible(true);
 		
 	}
+	
+	public void moveModel(int newX, int newY) {
+		monkeys.get(monkeys.size()-1).X = newX + 650;
+		monkeys.get(monkeys.size()-1).Y = newY + 25;
+		repaint();
+	}
+	
 	
 	public void build_round_collection(int round_in) {
 		round_collection.add(new bloon(RED,spawnX-BLOON_SIZE,spawnY));
@@ -115,13 +183,17 @@ public class GamePanel extends JPanel implements ActionListener{
 	}
 	
 	
-	public void paint(Graphics g) { // CHANGE THIS PROCESS, BLOONS SHOULD HOLD OWN IMAGE (MAKE STATIC IMAGES)
+	public void paint(Graphics g) {
 		super.paint(g);
 		g.drawImage(map, 0,0,null);
 		if (running) {
 			for (int i = 0; i < round_collection.size(); ++i) {
 				g.drawImage(round_collection.get(i).sprite, round_collection.get(i).X, round_collection.get(i).Y, BLOON_SIZE, BLOON_SIZE, null);
 			}
+		}
+		if (model) {
+			for(int i = 0; i < monkeys.size(); ++i)
+			g.drawImage(monkeys.get(i).sprite, monkeys.get(i).X, monkeys.get(i).Y, 50,50, null);
 		}
 		
 	}
